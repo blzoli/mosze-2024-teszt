d57f616aa0c72d9f82534b3bcd02097706489540
Zoltán Blahovics <blahovics.zoltan@hallgato.sze.hu>
2024-09-08 13:14:48 +0200

    Hibák kommentezése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // elírás: N_ELEMENTS 
+    std::cout << '1-100 ertekek duplazasa' // hiányzik a pontosvessző, aposztróf helyett idézőjel kell
+    for (int i = 0;) // for ciklus nincs megfelelően definiálva, csak az i van inicializálva
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // hibás a feltétel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // hiányzik az, hogy mit írjon ki, meg a pontosvessző
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // nincs inicializálva helyesen
+    for (int i = 0; i < N_ELEMENTS, i++) // hibás a vessző, pontosvessző kell
     {
-        atlag += b[i]
+        atlag += b[i] // hiányzik a pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
